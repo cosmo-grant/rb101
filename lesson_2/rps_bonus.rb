7b1c5bbbd7a6608557a16593a87c383aa03d3ecd
Cosmo Grant <cosmodgrant@gmail.com>
2022-03-01 14:54:06 -0800

    Minor refactoring

--- lesson_2/rps_bonus.rb
+++ lesson_2/rps_bonus.rb
@@ -13,11 +13,13 @@ def prompt(message)
 end
 
 def normalize_choice(choice)
-  return 'rock' if %w(rock r).include?(choice)
-  return 'paper' if %w(paper p).include?(choice)
-  return 'scissors' if %w(scissors sc).include?(choice)
-  return 'lizard' if %w(lizard l).include?(choice)
-  return 'spock' if %w(spock sp).include?(choice)
+  case choice
+  when 'rock', 'r' then 'rock'
+  when 'paper', 'p' then 'paper'
+  when 'scissors', 'sc' then 'scissors'
+  when 'lizard', 'l' then 'lizard'
+  when 'spock', 'sp' then 'spock'
+  end
 end
 
 def result(player_choice, computer_choice)
@@ -37,53 +39,65 @@ def display_results(result, player_tally, computer_tally)
   when -1 then prompt("Computer won.")
   end
 
-  prompt("Your score: #{player_tally}. Computer's score: #{computer_tally}.")
+  prompt("You: #{player_tally}. Computer: #{computer_tally}.")
 end
-
-player_tally = 0
-computer_tally = 0
-round = 1
 
 loop do
   puts
-  prompt("-----ROUND NUMBER #{round}-----")
+  prompt("Welcome!")
+  prompt("RPSLS, first to three. Here we go.")
 
-  # get player's choice
-  player_choice = nil
+  player_tally = 0
+  computer_tally = 0
+  round = 1
+
+  # first-to-three loop
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}.")
-    prompt("You can enter just the first letter or two if you like.")
-    prompt("e.g. 'p' for 'paper' or 'sp' for 'spock'")
-    player_choice = gets.chomp
-    player_choice = normalize_choice(player_choice)
-    if player_choice
-      break
+    puts
+    prompt("-----ROUND NUMBER #{round}-----")
+
+    player_choice = nil
+    # input validation loop
+    loop do
+      prompt("Choose one: #{VALID_CHOICES.join(', ')}.")
+      prompt("You can enter just the first letter or two.")
+      prompt("e.g. p -> paper, sc -> scissors, sp -> spock")
+      player_choice = gets.chomp
+      player_choice = normalize_choice(player_choice)
+      if player_choice
+        break
+      end
+      puts "Invalid choice."
     end
-    puts "That's not a valid choice."
+
+    computer_choice = VALID_CHOICES.sample
+
+    prompt("You: #{player_choice}. Computer: #{computer_choice}.")
+
+    result = result(player_choice, computer_choice)
+
+    case result
+    when 1 then player_tally += 1
+    when -1 then computer_tally += 1
+    end
+    round += 1
+
+    display_results(result, player_tally, computer_tally)
+
+    break if player_tally == 3 || computer_tally == 3
   end
 
-  computer_choice = VALID_CHOICES.sample
-
-  prompt("You chose #{player_choice}. Computer chose #{computer_choice}.")
-
-  result = result(player_choice, computer_choice)
-
-  case result
-  when 1 then player_tally += 1
-  when -1 then computer_tally += 1
+  puts
+  if player_tally == 3
+    prompt("You won the match.")
+  else
+    prompt("You lost the match.")
   end
-  round += 1
 
-  display_results(result, player_tally, computer_tally)
-
-  break if player_tally == 3 || computer_tally == 3
+  puts
+  prompt("Play again?")
+  response = gets.chomp
+  break unless response.downcase.start_with?('y')
 end
 
-puts
-if player_tally == 3
-  prompt("You're the winner.")
-else
-  prompt("The computer is the winner.")
-end
-
-prompt("Goodbye")
+prompt("Bye")
